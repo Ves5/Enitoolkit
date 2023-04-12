29c29c91d8c209f0ab5be16cc8c8e2e18cc1634d
Kacper Ochotny <trinerxd@gmail.com>
2023-04-12 23:51:50 +0200

    caesar and vigenere ciphers

--- Enitools-fe/src/app/app.module.ts
+++ Enitools-fe/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AnagramComponent } from './components/anagram/anagram.component';
 
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { SplitPipe } from './split-pipe.pipe';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { FormsModule } from '@angular/forms';
     CodesComponent,
     LinksComponent,
     TextInputTitledComponent,
-    AnagramComponent
+    AnagramComponent,
+    SplitPipe
   ],
   imports: [
     BrowserModule,
