313b038b36abcbfc939a7803b039e726a489bd2c
Kacper Ochotny <trinerxd@gmail.com>
2024-10-12 10:27:43 +0200

    angular 17 to 18 version bump phase 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { LinksComponent } from './components/links/links.component';
 import { TextInputTitledComponent } from './components/utility/text-input-titled/text-input-titled.component';
 import { AnagramComponent } from './components/anagram/anagram.component';
 
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { SplitPipe } from './split-pipe.pipe';
 import { PopupHelpComponent } from './components/utility/popup-help/popup-help.component';
@@ -31,41 +31,34 @@ import { MatDividerModule } from '@angular/material/divider';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatExpansionModule } from '@angular/material/expansion';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    CiphersComponent,
-    HomeComponent,
-    ConversComponent,
-    CodesComponent,
-    LinksComponent,
-    AnagramComponent,
-    SplitPipe,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    TextInputTitledComponent,
-    FormsModule,
-    PopupHelpComponent,
-    MatSlideToggleModule,
-    LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
-    BrowserAnimationsModule,
-    MatInputModule,
-    MatFormFieldModule,
-    MatSliderModule,
-    MatDividerModule,
-    MatProgressSpinnerModule,
-    MatExpansionModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        NavbarComponent,
+        CiphersComponent,
+        HomeComponent,
+        ConversComponent,
+        CodesComponent,
+        LinksComponent,
+        AnagramComponent,
+        SplitPipe,
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        TextInputTitledComponent,
+        FormsModule,
+        PopupHelpComponent,
+        MatSlideToggleModule,
+        LayoutModule,
+        MatToolbarModule,
+        MatButtonModule,
+        MatSidenavModule,
+        MatIconModule,
+        MatListModule,
+        BrowserAnimationsModule,
+        MatInputModule,
+        MatFormFieldModule,
+        MatSliderModule,
+        MatDividerModule,
+        MatProgressSpinnerModule,
+        MatExpansionModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
